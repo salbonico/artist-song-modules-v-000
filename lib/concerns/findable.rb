1a8ae79fc3767a66ddb67f36f97f10738ffb97f5
Steve Albonico <salbonico@gmail.com>
2019-01-19 02:04:38 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,12 @@
 module findable
-  
+
 
   def find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
 
   def find_by_name(song)
-    @@songs.detect{|a| a.song == song}
+    self.all.detect{|a| a.song == song}
   end
 
 end
