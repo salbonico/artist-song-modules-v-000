c2c03d613f5de7528d6a545bb061ef8836e6b1f5
Steve Albonico <salbonico@gmail.com>
2019-01-19 02:20:18 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,11 +2,11 @@ module Findable
 
 
   def find_by_name(name)
-    self.all.detect{|a| a.name == name}
+    self.all.detect{|a| a.names == name}
   end
 
   def find_by_name(song)
-    self.all.detect{|a| a.song == song}
+    self.all.detect{|a| a.songs == song}
   end
 
 end
