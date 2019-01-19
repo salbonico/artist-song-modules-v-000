ea84eaff2be4b2987a93fa833f2181d449e913e9
Steve Albonico <salbonico@gmail.com>
2019-01-19 01:51:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Artist
+extend Memorable
+
   attr_accessor :name
   attr_reader :songs
 
@@ -19,7 +21,7 @@ class Artist
     @@artists
   end
 
-  
+
 
   def add_song(song)
     @songs << song
